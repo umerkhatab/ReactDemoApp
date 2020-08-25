b5ed69c41b53781d9266de6b3461888d86ee3346
Umer Khatab <abbasiumerkhatab@gmail.com>
2020-08-25 12:32:53 +0500

    React demo Application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Dinner></Dinner> */}
+      {/* Now Ia am going to pass data from parent to child */}
+      <Dinner dishName="Chicken karahi" sweetDish="kheer" />
+      <hr />
+      <Dinner dishName="Chicken Biryani" sweetDish="Jaleebi" />
     </div>
   );
 }
